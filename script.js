169f13f6df41033acc7fb4279dd2f9c150903600
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-01-28 22:00:30 +0530

    porject completed.

--- script.js
+++ script.js
@@ -6,11 +6,31 @@ var insiderect = (details.clientX - rectanglelocation.left);
 
    
 if(insiderect < rectanglelocation.width/2){
-    console.log("left");
+var redcolor = gsap.utils.mapRange (0,
+    rectanglelocation.width/2,
+    255,
+    0,insiderect)
+    gsap.to(rect,{
+    backgroundColor:`rgb(${redcolor},0,0)`,
+    ease: Power4,
+})
 }
 
-else{
-    console.log("right");
-    
+else{  
+    var bluecolor = gsap.utils.mapRange (
+        rectanglelocation.width/2,
+        rectanglelocation.width,
+        0,
+        255,insiderect)
+        gsap.to(rect,{
+        backgroundColor:`rgb(0,0,${bluecolor})`,
+        // ease: Power4,
+    })
 }
 })
+rect.addEventListener("mouseleave",function(){
+    gsap.to(rect,{
+        backgroundColor : "white"
+    })
+})
+
